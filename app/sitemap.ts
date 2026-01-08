511cb48dba0f6f3027c559f8c5ab6b7763c5daaf
MD-Tauhid <tauhid.swe@gmail.com>
2026-01-08 15:25:08 +0600

    update

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,8 +3,10 @@ import { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://tauhidur-rahman.vercel.app",
+      url: "https://tauhidur-rahman.vercel.app/",
       lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 1,
     },
   ];
 }
